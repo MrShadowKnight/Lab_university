e789c73a30f3e5baed62f8637d6d9a179a6a2b63
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-09-23 14:20:32 +0300

    Add.changes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #define _USE_MATH_DEFINES
 #include <math.h>
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    double a;
+    cout << "a = ";
+    cin >> a;
+
+    double z1 = (1 - 2 * sin(a) * sin(a)) / (1 + sin(2 * a));
+
+    double z2 = (1 - tan(a)) / (1 + tan(a));
+
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
 }
