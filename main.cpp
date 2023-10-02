de5dc82b1a5585f659000f4c56eddf7cd5581b92
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-10-02 12:26:43 +0300

    Add.changes

--- main.cpp
+++ main.cpp
@@ -12,16 +12,38 @@ int main()
     cout << "x = ";
     cin >> x;
 
+    // Спосіб 1
     if (x <= -1)
     {
         A = log10(fabs(cos(5 * x))) + exp(1 / x + x);
     }
-    else if (x > -1 && x < 0.4)
+    if (x > -1 && x < 0.4)
     {
-        A = ((2 - x) * (2 - x) * (2 - x)) / ((2 - x) * (2 - x) * (2 - x)) - tan(x);
+        A = sqrt(pow((2 - x), 3)) - tan(x);
     }
-    else if (x >= 0.4)
+    if (x >= 0.4)
     {
-        A = sin(5 * x) - (fabs(1 - x)) / (fabs(1 - x));
+        A = sin(5 * x) - sqrt(abs(1 - x));
     }
+    y = x + A;
+    cout << "1) y = " << A << endl;
+
+    // Спосіб 2
+    if (x <= -1)
+    {
+        A = log10(fabs(cos(5 * x))) + exp(1 / x + x);
+    }
+    else
+    {
+        if (x > -1 && x < 0.4)
+        {
+            A = sqrt(pow((2 - x), 3)) - tan(x);
+        }
+        else
+        {
+            A = sin(5 * x) - sqrt(abs(1 - x));
+        }
+    }
+    y = x + A;
+    cout << "2) y = " << A;
 }
